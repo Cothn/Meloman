84cdf3ca38f89ac1629c04f5c7abac21f49446c4
Cothn <48108190+Cothn@users.noreply.github.com>
2020-05-03 21:11:11 +0300

    fix viewController

--- backend/controllers/usersViewController.js
+++ backend/controllers/usersViewController.js
@@ -1,14 +1,14 @@
 
 exports.createUser = function (request, response){
-    response.status(200).render("create.hbs");
+    response.status(200).redirect("/static/create.html");
 };
 
 exports.editUser = function (request, response){
-    response.status(200).render("edit.hbs");
+    response.status(200).redirect("/static/edit.html");
 };
 
 exports.showUsers = function(request, response){
 
-    response.status(200).render("users.hbs");
+    response.status(200).redirect("/static/users.html");
 
 };
