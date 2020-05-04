55350bfa9a9cf663b345f9c7305a200f71df80b0
Cothn <48108190+Cothn@users.noreply.github.com>
2020-05-04 12:09:45 +0300

    modifide sendfile to sendFile

--- backend/controllers/usersViewController.js
+++ backend/controllers/usersViewController.js
@@ -1,13 +1,16 @@
 const logger = require('../configs/logger4jsInit');
+const path = require('path');
+const root_path=path.join(__dirname, '../static/views');
 
 exports.createUser = function (request, response){
-    response.status(200).sendfile("static/views/create.html");
+    //logger.debug(path.join(__dirname, '../static/views', 'create.html'));
+    response.status(200).sendFile( 'create.html', {root: root_path});
 };
 
 exports.editUser = function (request, response){
-    response.status(200).sendfile("static/views/edit.html");
+    response.status(200).sendFile( 'edit.html', {root: root_path});
 };
 
 exports.showUsers = function(request, response){
-    response.status(200).sendfile("static/views/users.html");
+    response.status(200).sendFile( 'users.html', {root: root_path});
 };
